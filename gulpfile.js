fb85c4c21203b68485d93bb737878ea82b1304e0
Bios Sun <biossun@gmail.com>
2020-05-03 14:46:15 +0800

    add color parse benchmark.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+const ts = require('gulp-typescript')
+const shelljs = require('shelljs')
+
+gulp.task('benchmark', () => {
+    const tsp = ts.createProject('./tsconfig.json')
+
+    return gulp
+        .src(['index.ts', 'index.benchmark.ts'])
+        .pipe(tsp())
+        .pipe(gulp.dest('./output'))
+        .on('end', () => {
+            shelljs.cd(__dirname)
+            shelljs.exec('node ./output/index.benchmark.js')
+        })
+})
---
+++ index.benchmark.ts
@@ -0,0 +1,24 @@
+import { Suite } from 'benchmark'
+import * as colorString from 'color-string'
+import color from './index'
+
+function bench(value: string) {
+    console.log(`\n> ${value}`)
+
+    new Suite()
+        .add('@nami/color    ', () => color(value))
+        .add('colorString    ', () => colorString.get(value))
+        .on('cycle', (event: Event) => console.log('  ' + event.target))
+        .run()
+
+    console.log('')
+}
+
+bench('#FFF')
+bench('#FFFF')
+bench('#FFFFFF')
+bench('#FFFFFFFF')
+bench('rgb(255, 255, 255)')
+bench('rgba(255, 255, 255, 1)')
+bench('hsl(0, 100%, 50%)')
+bench('hsla(0, 100%, 50%, 1)')
